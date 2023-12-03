e3617ebdbc6803fdb72f6cb2e065c9f0d2a80451
Aquasonic1995 <Aquasonic1995@gmail.com>
2023-12-03 11:05:59 +0300

    feat:add fade-in and fade-out on radiobutton switch

--- src/Components/Favorites/Favorites.tsx
+++ src/Components/Favorites/Favorites.tsx
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Favorites.module.scss'
 import data from "../../Redux/store";
+import cn from "clsx";
 
 function Favorites() {
+    const [season, setSeason] = useState(data.winter)
+    const [fade, setFade] = useState(false);
+    useEffect(() => {
+
+        // Remove fade-in class after a delay (adjust the duration accordingly)
+        const timeout = setTimeout(() => {
+            setFade(true);
+        });
+
+        // Clear the timeout on component unmount or when the season changes
+        return () => clearTimeout(timeout);
+    }, [season]);
+    const changeSeasonWinter = () => {
+       setFade(false)
+        setTimeout(()=>{setSeason(data.winter)},500)
+
+    }
+    const changeSeasonSpring = () => {
+        setFade(false)
+        setTimeout(()=>{  setSeason(data.spring)},500)
+
+    }
+    const changeSeasonSummer = () => {
+        setFade(false)
+        setTimeout(()=>{ setSeason(data.summer)  },500)
+
+    }
+    const changeSeasonAutumn = () => {
+        setFade(false)
+        setTimeout(()=>{   setSeason(data.autumn)  },500)
+
+    }
+
     return (
         <section className={s.favorites} id="favorites">
             <h2>Favorites</h2>
             <div className={s.pick}>Pick favorites of season</div>
             <ul className={s.seasons}>
-                <label htmlFor="r1">
-                    <li><input type="radio"  id='r1' name='year'/>Winter</li>
+                <label htmlFor="winter">
+                    <li><input type="radio" id='winter' name='year' onChange={changeSeasonWinter} defaultChecked/>Winter</li>
                 </label>
-                <label htmlFor="r2">
-                    <li><input type="radio" id='r2'  name='year'/>Spring</li>
+                <label htmlFor="spring">
+                    <li><input type="radio" id='spring' name='year' onChange={changeSeasonSpring}/>Spring</li>
                 </label>
-                <label htmlFor="r3">
-                    <li><input type="radio" id="r3"  name='year'/>Summer</li>
+                <label htmlFor="summer">
+                    <li><input type="radio" id="summer" name='year' onChange={changeSeasonSummer}/>Summer</li>
                 </label>
-                <label htmlFor="r4">
-                    <li><input type="radio" id='r4'  name='year'/>Autumn</li>
+                <label htmlFor="autumn">
+                    <li><input type="radio" id='autumn' name='year' onChange={changeSeasonAutumn}/>Autumn</li>
                 </label>
             </ul>
-            <div className={s.books}>
-                {data.map((book,i) => (
+            <div className={`${s.books} ${fade ? s.fadeIn : s.fadeOut}`}>
+                {season.map((book, i) => (
                     <div className={s.book} key={i}>
                         <div className={s.staff}>Staff Picks</div>
                         <div className={s.title}>{book.title}</div>
-                        <div className={s.author}>By  {book.author}</div>
+                        <div className={s.author}>By {book.author}</div>
                         <div className={s.annotation}>{book.annotation}</div>
                         <button>{book.button}</button>
                         <img src={require('../../img/' + book.img + '.png')} alt={book.img}/>
--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,4 +1,4 @@
-type DataType = Array<BookType>
+ export  type SeasonType = Array<BookType>
 type BookType ={
     title:string
     author:string
@@ -6,37 +6,137 @@ type BookType ={
     img?:any
     button:string
 }
+type DataType = {
+    winter:SeasonType,
+    spring:SeasonType,
+    summer:SeasonType,
+    autumn:SeasonType
+}
+const data: DataType = {
+    winter:[
+        {
+            title: 'The Book Eaters',
+            author: 'Sunyi Dean',
+            annotation: 'An unusual sci-fi story about a book eater woman who tries desperately to save her dangerous mind-eater son from tradition and certain death. Complete with dysfunctional family values, light Sapphic romance, and a strong, complex protagonist. Not for the faint of heart.',
+            img:'book1',
+            button:'Buy'
+        },
+        {
+            title: 'Cackle',
+            author: 'Rachel Harrison',
+            annotation: 'Are your Halloween movies of choice The Witches of Eastwick and Practical Magic? Look no further than here - where a woman recovering from a breakup moves to a quaint town in upstate New York and befriends a beautiful witch.',
+            img:'book2',
+            button:'Buy'
+        },
+        {
+            title: 'Dante: Poet of the secular world',
+            author: 'Erich Auerbach',
+            annotation: 'Auerbach\'s engaging book places the \'Comedy\' within the tradition of epic, tragedy, and philosophy in general, arguing for Dante\'s uniqueness as one who raised the individual and his drama of soul into something of divine significance—an inspired introduction to Dante\'s main themes.',
+            img:'book3',
+            button:'Buy'
+        },
+        {
+            title: 'The Last Queen',
+            author: 'Clive Irving',
+            annotation: 'A timely and revelatory new biography of Queen Elizabeth (and her family) exploring how the Windsors have evolved and thrived as the modern world has changed around them.',
+            img:'book4',
+            button:'Own'
+        }
 
-const data: DataType = [
-    {
-        title: 'The Book Eaters',
-        author: 'Sunyi Dean',
-        annotation: 'An unusual sci-fi story about a book eater woman who tries desperately to save her dangerous mind-eater son from tradition and certain death. Complete with dysfunctional family values, light Sapphic romance, and a strong, complex protagonist. Not for the faint of heart.',
-        img:'book1',
-        button:'Buy'
-    },
-    {
-        title: 'Cackle',
-        author: 'Rachel Harrison',
-        annotation: 'Are your Halloween movies of choice The Witches of Eastwick and Practical Magic? Look no further than here - where a woman recovering from a breakup moves to a quaint town in upstate New York and befriends a beautiful witch.',
-        img:'book2',
-        button:'Buy'
-    },
-    {
-        title: 'Dante: Poet of the secular world',
-        author: 'Erich Auerbach',
-        annotation: 'Auerbach\'s engaging book places the \'Comedy\' within the tradition of epic, tragedy, and philosophy in general, arguing for Dante\'s uniqueness as one who raised the individual and his drama of soul into something of divine significance—an inspired introduction to Dante\'s main themes.',
-        img:'book3',
-        button:'Buy'
-    },
-    {
-        title: 'The Last Queen',
-        author: 'Clive Irving',
-        annotation: 'A timely and revelatory new biography of Queen Elizabeth (and her family) exploring how the Windsors have evolved and thrived as the modern world has changed around them.',
-        img:'book4',
-        button:'Own'
-    }
-]
+    ],
+    spring:[
+        {
+            title: 'The Body',
+            author: 'Stephen King',
+            annotation: 'Powerful Novel That Takes You Back To A Nostalgic Time, Exploring Both The Beauty And Danger And Loss Of Innocence That Is Youth.',
+            img:'book1',
+            button:'Buy'
+        },
+        {
+            title: 'CARRY: A MEMOIR OF SURVIVAL ON STOLEN LAND',
+            author: 'Toni Jenson',
+            annotation: 'This Memoir About The Author\'s Relationship With Gun Violence Feels Both Expansive And Intimate, Resulting In A Lyrical Indictment Of The Way Things Are.',
+            img:'book2',
+            button:'Buy'
+        },
+        {
+            title: 'DAYS OF DISTRACTION',
+            author: 'Alexandra Chang',
+            annotation: 'A Sardonic View Of Silicon Valley Culture, A Meditation On Race, And A Journal Of Displacement And Belonging, All In One Form-Defying Package Of Spare Prose.',
+            img:'book3',
+            button:'Buy'
+        },
+        {
+            title: 'DOMINICANA',
+            author: 'Angie Cruz',
+            annotation: 'A Fascinating Story Of A Teenage Girl Who Marries A Man Twice Her Age With The Promise To Bring Her To America. Her Marriage Is An Opportunity For Her Family To Eventually Immigrate. For Fans Of Isabel Allende And Julia Alvarez.',
+            img:'book4',
+            button:'Own'
+        }
+
+    ],
+    summer:[
+        {
+            title: 'CRUDE: A MEMOIR',
+            author: 'Pablo Fajardo & ​​Sophie Tardy-Jouber',
+            annotation: 'Drawing And Color By Damien Roudeau | This Book Illustrates The Struggles Of A Group Of Indigenous Ecuadoreans As They Try To Sue The ChevronTexaco Company For Damage Their Oil Fields Did To The Amazon And Her People.',
+            img:'book1',
+            button:'Buy'
+        },
+        {
+            title: 'LET MY PEOPLE GO SURFING',
+            author: 'Yvon Chouinard',
+            annotation: 'Chouinard—Climber, Businessman, Environmentalist—Shares Tales Of Courage And Persistence From His Experience Of Founding And Leading Patagonia, Inc. Full Title: Let My People Go Surfing: The Education Of A Reluctant Businessman, Including 10 More Years Of Business Unusual.',
+            img:'book2',
+            button:'Buy'
+        },
+        {
+            title: 'THE OCTOPUS MUSEUM: POEMS',
+            author: 'Brenda Shaughnessy',
+            annotation: 'This Collection Of Bold And Scathingly Beautiful Feminist Poems Imagines What Comes After Our Current Age Of Environmental Destruction, Racism, Sexism, And Divisive Politics',
+            img:'book3',
+            button:'Buy'
+        },
+        {
+            title: 'SHARK DIALOGUES: A NOVEL',
+            author: 'Kiana Davenport',
+            annotation: 'An Epic Saga Of Seven Generations Of One Family Encompasses The Tumultuous History Of Hawaii As A Hawaiian Woman Gathers Her Four Granddaughters Together In An Erotic Tale Of Villains And Dreamers, Queens And Revolutionaries, Lepers And Healers.',
+            img:'book4',
+            button:'Own'
+        }
+    ],
+    autumn: [
+        {
+            title: 'CASUAL CONVERSATION',
+            author: 'Renia White',
+            annotation: 'White\'s Impressive Debut Collection Takes Readers Through And Beyond The Concepts Of Conversation And The Casual - Both What We Say To Each Other And What We Don\'t, Examining The Possibilities Around How We Construct And Communicate Identity.',
+            img:'book1',
+            button:'Buy'
+        },
+        {
+            title: 'THE GREAT FIRE',
+            author: 'Lou Ureneck',
+            annotation: 'The Harrowing Story Of An Ordinary American And A Principled Naval Officer Who, Horrified By The Burning Of Smyrna, Led An Extraordinary Rescue Effort That Saved A Quarter Of A Million Refugees From The Armenian Genocide',
+            img:'book2',
+            button:'Buy'
+        },
+        {
+            title: 'RICKEY: THE LIFE AND LEGEND',
+            author: 'Howard Bryant',
+            annotation: 'With The Fall Rolling Around, One Can\'t Help But Think Of Baseball\'s Postseason Coming Up! And What Better Way To Prepare For It Than Reading The Biography Of One Of The Game\'s All-Time Greatest Performers, The Man Of Steal, Rickey Henderson?',
+            img:'book3',
+            button:'Buy'
+        },
+        {
+            title: 'SLUG: AND OTHER STORIES',
+            author: 'Megan Milks',
+            annotation: 'Exes Tegan And Sara Find Themselves Chained Together By Hairballs Of Codependency. A Father And Child Experience The Shared Trauma Of Giving Birth To Gods From Their Wounds.',
+            img:'book4',
+            button:'Own'
+        }
+    ]
+
+}
 export const coffeeTea =[
     {
         title: 'Cold Brew Coffee',
